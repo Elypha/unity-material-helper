2f1b8b949872143630214558e67a39ea50837ab7
Elypha Grey <i@elypha.com>
2025-07-14 03:53:38 +0900

    reuse DrawAdvancedSettings

--- Editor/Helper/UnityHelper.cs
+++ Editor/Helper/UnityHelper.cs
@@ -118,6 +118,32 @@ namespace Elypha.Helper
             Separator(Color.grey, 1, 0, 4);
         }
 
+
+        public static void DrawAdvancedSettings(ref bool showAdvancedSettings, ref I18N.PluginLanguage language, ref I18N.TemplateI18N i18n)
+        {
+            showAdvancedSettings = EditorGUILayout.Foldout(
+                showAdvancedSettings,
+                "Advanced Settings",
+                true,
+                new GUIStyle(EditorStyles.foldout)
+                {
+                    fontStyle = FontStyle.Bold
+                }
+            );
+
+            if (showAdvancedSettings)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Language", GUILayout.Width(100));
+                language = (I18N.PluginLanguage)EditorGUILayout.EnumPopup(language, GUILayout.Width(200));
+                if (language != i18n.language)
+                {
+                    i18n = new I18N.TemplateI18N(language);
+                }
+                GUILayout.EndHorizontal();
+            }
+        }
+
     }
 
     public static class Utils
@@ -132,6 +158,7 @@ namespace Elypha.Helper
             return result;
         }
     }
+
 }
 
 public static class TransformExtensions
