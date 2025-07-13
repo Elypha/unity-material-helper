41825f9868b33a9950719b2007bbcd53fd809591
Elypha Grey <i@elypha.com>
2025-07-13 15:04:47 +0900

    add object name override in material swapper

--- Editor/Helper/UnityHelper.cs
+++ Editor/Helper/UnityHelper.cs
@@ -119,6 +119,19 @@ namespace Elypha.Helper
         }
 
     }
+
+    public static class Utils
+    {
+        public static string ReplaceLastOccurrence(string source, string find, string replace)
+        {
+            int place = source.LastIndexOf(find);
+
+            if (place == -1) return source;
+
+            string result = source.Remove(place, find.Length).Insert(place, replace);
+            return result;
+        }
+    }
 }
 
 public static class TransformExtensions
