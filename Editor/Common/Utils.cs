14220806d5d93f05a819daf7fdb52056128f3cb2
Elypha Grey <i@elypha.com>
2025-07-16 10:44:55 +0900

    add texture manager

--- Editor/Common/Utils.cs
+++ Editor/Common/Utils.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
 namespace Elypha.Common
 {
     public static class Utils
     {
+        public static readonly GUIStyle MonoStyle = GetMonoStyle();
+
         public static string ReplaceLastOccurrence(string source, string find, string replace)
         {
             int place = source.LastIndexOf(find);
@@ -10,6 +18,23 @@ namespace Elypha.Common
 
             string result = source.Remove(place, find.Length).Insert(place, replace);
             return result;
+        }
+
+        public static GUIStyle GetMonoStyle()
+        {
+            var monoStyle = new GUIStyle()
+            {
+                fontSize = 12,
+                normal = { textColor = Color.white }
+            };
+
+            var robotoFont = EditorGUIUtility.Load("Fonts/RobotoMono/RobotoMono-Regular.ttf") as Font;
+            if (robotoFont != null)
+                monoStyle.font = robotoFont;
+            else
+                Debug.LogWarning("RobotoMono-Regular.ttf not found. Using default font style.");
+
+            return monoStyle;
         }
     }
 
